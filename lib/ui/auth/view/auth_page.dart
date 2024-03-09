5154cba351e0c4aef793e9cf77396af768c93c9a
Reimanbow <reimanbow0318@gmail.com>
2024-03-09 23:37:42 +0900

    ミスを修正

--- lib/ui/auth/view/auth_page.dart
+++ lib/ui/auth/view/auth_page.dart
@@ -3,7 +3,7 @@ import "package:flutter_riverpod/flutter_riverpod.dart";
 
 import "../../../provider/provider.dart";
 import "signin_page.dart";
-import "../todo_list/view/todo_list_page.dart";
+import '../../todo_list/view/todo_list_page.dart';
 
 class AuthPage extends StatelessWidget {
   const AuthPage({super.key});
