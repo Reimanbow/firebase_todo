a28873ce3cda639013eceea8343dba847ca66f33
Reimanbow <reimanbow0318@gmail.com>
2024-03-11 22:09:02 +0900

    todo_view_modelの追加

--- lib/provider/provider.dart
+++ lib/provider/provider.dart
@@ -1,7 +1,17 @@
 import "package:flutter_riverpod/flutter_riverpod.dart";
 
 import "../ui/auth/view_model/auth_view_model.dart";
+import "../ui/todo_list/view_model/todo_list_view_model.dart";
+import "../ui/todo_list/view_model/todo_view_model.dart";
 
 final authProvider = ChangeNotifierProvider((ref) {
   return AuthViewModel();
+});
+
+final todoListProvider = ChangeNotifierProvider((ref) {
+  return TodoListViewModel();
+});
+
+final todoProvider = Provider((ref) {
+  return TodoViewModel();
 });
