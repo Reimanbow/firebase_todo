5ac3fc601335957eb946976a904c7561724d26ef
Reimanbow <reimanbow0318@gmail.com>
2024-03-11 22:57:21 +0900

    ミスの修正

--- lib/ui/todo_list/view_model/todo_view_model.dart
+++ lib/ui/todo_list/view_model/todo_view_model.dart
@@ -1,25 +1,17 @@
 import "../../../model/todo/todo_model.dart";
 
 class TodoViewModel {
-  TodoViewModel();
+  TodoViewModel(this._todo);
 
-  Todo? _todo;
+  final Todo _todo;
 
-  void setTodo(Todo todo) {
-    _todo = todo;
-  }
+  Todo get todo => _todo;
 
-  Todo? get todo => _todo;
+  String get todoId => _todo.todoId;
 
-  String get todoId => _todo!.todoId;
+  String get title => _todo.title;
 
-  String get title => _todo!.title;
+  String get description => _todo.description;
 
-  String get description => _todo!.description;
-
-  bool get isDone => _todo!.isDone;
-
-  DateTime get dueDateTime => _todo!.dueDateTime;
-
-  DateTime get createdDateTime => _todo!.createdDateTime;
+  bool get isDone => _todo.isDone;
 }
