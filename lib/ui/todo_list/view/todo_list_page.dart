2a1ea3f20a259a3e2af830e8ca0e6d471347738a
Reimanbow <reimanbow0318@gmail.com>
2024-03-12 01:08:39 +0900

    todo作成画面作成

--- lib/ui/todo_list/view/todo_list_page.dart
+++ lib/ui/todo_list/view/todo_list_page.dart
@@ -4,12 +4,16 @@ import "package:flutter_riverpod/flutter_riverpod.dart";
 import '../../../provider/provider.dart';
 import "../view_model/todo_view_model.dart";
 import "todo_list_item.dart";
+import "../../todo_create/view/todo_create_page.dart";
 
-class TodoListPage extends StatelessWidget {
+class TodoListPage extends ConsumerWidget {
   const TodoListPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final firestoreProvider = ref.watch(todoListProvider);
+    final todoList = firestoreProvider.todoData;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text(
@@ -22,38 +26,40 @@ class TodoListPage extends StatelessWidget {
         actions: [
           ElevatedButton(
             child: const Text("create todo"),
-            //TODO 作成ページに移動する
-            onPressed: () {},
+            // 作成ページに移動する
+            onPressed: () async {
+              await Navigator.of(context).push<void>(
+                MaterialPageRoute(
+                  builder: (context) => const TodoCreatePage(),
+                ),
+              );
+
+              if (!context.mounted) {
+                return;
+              }
+
+              await ref.read(todoListProvider).refresh();
+            },
           ),
           const SizedBox(
             width: 10,
           ),
-          Consumer(
-            builder: (context, ref, child) {
-              return ElevatedButton(
-                child: const Text("sign out"),
-                onPressed: () {
-                  final auth = ref.read(authProvider);
-                  auth.signOut();
-                },
-              );
+          ElevatedButton(
+            child: const Text("sign out"),
+            onPressed: () {
+              final auth = ref.read(authProvider);
+              auth.signOut();
             },
           ),
         ],
       ),
       body: Padding(
         padding: const EdgeInsets.all(20),
-        child: Consumer(
-          builder: (context, ref, child) {
-            final firestoreProvider = ref.watch(todoListProvider);
-            final todoList = firestoreProvider.todoData;
-            return ListView.builder(
-              itemCount: todoList.length,
-              itemBuilder: (context, index) {
-                return TodoListItem(
-                  todoViewModel: TodoViewModel(todoList[index]),
-                );
-              },
+        child: ListView.builder(
+          itemCount: todoList.length,
+          itemBuilder: (context, index) {
+            return TodoListItem(
+              todoViewModel: TodoViewModel(todoList[index]),
             );
           },
         ),
